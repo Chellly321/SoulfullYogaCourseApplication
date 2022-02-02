27d05fab73500db29da5693a2ccbfad42a045bfd
Chelly321 <michelle.royer@icloud.com>
2022-02-02 12:52:18 -0400

    first commit

---
+++ src/main/java/nl/novi/soulfullyogacourseapplication/controllers/UserController.java
@@ -0,0 +1,18 @@
+package nl.novi.soulfullyogacourseapplication.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "user")
+public class UserController {
+
+    //get-request for all tv's
+
+
+    }
+
---
+++ src/main/java/nl/novi/soulfullyogacourseapplication/model/User.java
@@ -0,0 +1,4 @@
+package nl.novi.soulfullyogacourseapplication.model;
+
+public class User {
+}
